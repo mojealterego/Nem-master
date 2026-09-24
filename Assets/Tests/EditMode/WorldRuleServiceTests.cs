cb18ddbdbce797baebb84c80326fb9f527404dd2
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 23:18:33 +0200

    Add world boss and seasonal rule foundation

---
+++ Assets/Tests/EditMode/WorldRuleServiceTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using UnityEngine;
+using NewMaster.Worlds;
+namespace NewMaster.Tests
+{
+    public sealed class WorldRuleServiceTests
+    {
+        [Test]
+        public void WorldMetadataControlsBossAndSeasonalFlags()
+        {
+            var world = ScriptableObject.CreateInstance<WorldDefinition>();
+            try
+            {
+                world.bossId = "boss.001";
+                world.seasonalTag = "winter";
+                var service = new WorldRuleService();
+                Assert.That(service.HasBoss(world), Is.True);
+                Assert.That(service.IsSeasonal(world), Is.True);
+                Assert.That(service.ApplyRewardMultiplier(world, 100), Is.EqualTo(100));
+            }
+            finally { Object.DestroyImmediate(world); }
+        }
+    }
+}
