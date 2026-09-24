db2fcbae2a88ff52ff35ea9de8979b877bbb913a
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 23:28:17 +0200

    Add runtime world catalog coverage

---
+++ Assets/Tests/EditMode/WorldCatalogRuntimeTests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using NewMaster.Worlds;
+using UnityEngine;
+
+namespace NewMaster.Tests
+{
+    public sealed class WorldCatalogRuntimeTests
+    {
+        [Test]
+        public void RuntimeFallbackCreatesRequestedWorldRange()
+        {
+            var catalog = WorldCatalog.CreateRuntimeFallback(365);
+
+            try
+            {
+                Assert.That(catalog.Worlds.Count, Is.EqualTo(365));
+                Assert.That(catalog.Find(1), Is.Not.Null);
+                Assert.That(catalog.Find(365), Is.Not.Null);
+                Assert.That(catalog.Find(366), Is.Null);
+                Assert.That(catalog.Find(365).bossId, Is.EqualTo("boss.365"));
+                Assert.That(catalog.Find(20).seasonalTag, Is.EqualTo("season.01"));
+            }
+            finally
+            {
+                Object.DestroyImmediate(catalog);
+            }
+        }
+    }
+}
