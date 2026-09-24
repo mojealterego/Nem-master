7c5031fb91d0c068aa00b35da27d7b0f214f6eaf
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 18:07:17 +0200

    Add centralized New Master localization keys

---
+++ Assets/Scripts/Localization/NewMasterTextKeys.cs
@@ -0,0 +1,23 @@
+namespace NewMaster.Localization
+{
+    public static class NewMasterTextKeys
+    {
+        public const string GameReady = "game.ready";
+        public const string NoReward = "game.no_reward";
+        public const string CoinsReward = "game.coins_reward";
+        public const string EnergyReward = "game.energy_reward";
+        public const string VillageProgress = "game.village_progress";
+        public const string NewWorld = "world.new_world";
+        public const string RaidLoot = "raid.loot";
+        public const string RaidShieldBlocked = "raid.shield_blocked";
+        public const string RaidNoToken = "raid.no_token";
+        public const string RaidEmptyTarget = "raid.empty_target";
+        public const string CollectionComplete = "collection.complete";
+        public const string CollectionIncomplete = "collection.incomplete";
+        public const string CoinsLabel = "hud.coins";
+        public const string EnergyLabel = "hud.energy";
+        public const string WorldLabel = "hud.world";
+        public const string VillageLabel = "hud.village";
+        public const string RaidTokensLabel = "hud.raid_tokens";
+    }
+}
