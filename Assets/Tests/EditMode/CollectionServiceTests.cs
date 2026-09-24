c5c8c46ec3b44f717aaa0611e8bfcb4f8fd2f705
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 17:37:01 +0200

    Add New Master collection tests

---
+++ Assets/Tests/EditMode/CollectionServiceTests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using NewMaster.Collections;
+using NewMaster.Core;
+
+namespace NewMaster.Tests
+{
+    public sealed class CollectionServiceTests
+    {
+        [Test]
+        public void CompletedSetGrantsReward()
+        {
+            var set = new CollectionSet
+            {
+                Id = "set-01",
+                DisplayName = "Pierwszy Album",
+                CompletionReward = 1000,
+                RequiredCards = { "card-a", "card-b" }
+            };
+            var state = new CollectionState();
+            state.OwnedCardIds.Add("card-a");
+            state.OwnedCardIds.Add("card-b");
+            var game = new GameState();
+
+            Assert.That(new CollectionService().TryComplete(set, state, game), Is.True);
+            Assert.That(game.Coins, Is.EqualTo(1000));
+        }
+
+        [Test]
+        public void IncompleteSetDoesNotGrantReward()
+        {
+            var set = new CollectionSet
+            {
+                Id = "set-02",
+                DisplayName = "Drugi Album",
+                CompletionReward = 1000,
+                RequiredCards = { "card-a", "card-b" }
+            };
+            var state = new CollectionState();
+            state.OwnedCardIds.Add("card-a");
+            var game = new GameState();
+
+            Assert.That(new CollectionService().TryComplete(set, state, game), Is.False);
+            Assert.That(game.Coins, Is.EqualTo(0));
+        }
+    }
+}
