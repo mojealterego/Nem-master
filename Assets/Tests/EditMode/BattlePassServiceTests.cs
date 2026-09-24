460cd41337559d73c5c5b6c8ff66dfed54d9d274
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 23:41:53 +0200

    Add BattlePassServiceTests.cs

---
+++ Assets/Tests/EditMode/BattlePassServiceTests.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using NewMaster.Core;
+using NewMaster.Monetization;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NewMaster.Tests
+{
+    public sealed class BattlePassServiceTests
+    {
+        [Test]
+        public void UnlockedTierCanBeClaimedOnce()
+        {
+            var definition = ScriptableObject.CreateInstance<BattlePassDefinition>();
+            definition.seasonId = "season-1";
+            definition.tiers = new List<BattlePassDefinition.Tier>
+            {
+                new BattlePassDefinition.Tier
+                {
+                    level = 1,
+                    experienceRequired = 100,
+                    coinReward = 500,
+                    energyReward = 2
+                }
+            };
+
+            var state = new BattlePassState { SeasonId = "season-1", Experience = 100 };
+            var gameState = new GameState();
+            var service = new BattlePassService();
+
+            try
+            {
+                Assert.That(service.TryClaimNext(definition, state, gameState), Is.True);
+                Assert.That(gameState.Coins, Is.EqualTo(500));
+                Assert.That(gameState.Energy, Is.EqualTo(12));
+                Assert.That(service.TryClaimNext(definition, state, gameState), Is.False);
+            }
+            finally
+            {
+                Object.DestroyImmediate(definition);
+            }
+        }
+    }
+}
