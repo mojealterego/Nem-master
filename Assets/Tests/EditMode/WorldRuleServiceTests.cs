32e66d27131459b2f2d70c10529d65db7bc06414
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 23:49:35 +0200

    Add world reward boundary coverage

--- Assets/Tests/EditMode/WorldRuleServiceTests.cs
+++ Assets/Tests/EditMode/WorldRuleServiceTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using UnityEngine;
 using NewMaster.Worlds;
+
 namespace NewMaster.Tests
 {
     public sealed class WorldRuleServiceTests
@@ -17,6 +18,20 @@ namespace NewMaster.Tests
                 Assert.That(service.HasBoss(world), Is.True);
                 Assert.That(service.IsSeasonal(world), Is.True);
                 Assert.That(service.ApplyRewardMultiplier(world, 100), Is.EqualTo(100));
+            }
+            finally { Object.DestroyImmediate(world); }
+        }
+
+        [Test]
+        public void RewardMultiplierSaturatesAtLongMaxValue()
+        {
+            var world = ScriptableObject.CreateInstance<WorldDefinition>();
+            try
+            {
+                world.rewardMultiplier = float.MaxValue;
+                Assert.That(
+                    new WorldRuleService().ApplyRewardMultiplier(world, long.MaxValue),
+                    Is.EqualTo(long.MaxValue));
             }
             finally { Object.DestroyImmediate(world); }
         }
