265445050bbdd784198b2bba080ff8dfbd3c7ff4
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 17:27:38 +0200

    Establish New Master player state model

--- Assets/Scripts/Core/GameState.cs
+++ Assets/Scripts/Core/GameState.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
 
-namespace NemMaster.Core
+namespace NewMaster.Core
 {
     [Serializable]
     public sealed class GameState
     {
+        public const int CurrentVersion = 1;
+
+        public int Version = CurrentVersion;
         public long Coins;
-        public int Energy;
+        public int Energy = 10;
         public int CurrentWorldId = 1;
+        public int CurrentVillageLevel = 1;
+        public int SpinsWon;
         public bool IsSpinning;
-        public string StatusMessage = "Gotowy do gry!";
+        public string StatusMessage = "New Master gotowy.";
         public List<string> Slots = new() { "?", "?", "?" };
     }
 }
