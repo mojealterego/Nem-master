4b543ecfd89dce0e2ead48e38075e8e1ee2a4f09
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 23:17:49 +0200

    Expand village state for damage defense and customization

--- Assets/Scripts/Village/VillageState.cs
+++ Assets/Scripts/Village/VillageState.cs
@@ -11,19 +11,25 @@ namespace NewMaster.Village
         {
             public int BuildingId;
             public int Level = 1;
+            public int Damage;
+            public bool HasDefense;
         }
 
         public List<BuildingProgress> Buildings = new();
+        public string CustomizationId = "default";
 
         public int GetLevel(int buildingId)
         {
             for (var i = 0; i < Buildings.Count; i++)
-            {
-                if (Buildings[i].BuildingId == buildingId)
-                    return Buildings[i].Level;
-            }
-
+                if (Buildings[i].BuildingId == buildingId) return Buildings[i].Level;
             return 1;
+        }
+
+        public int GetDamage(int buildingId)
+        {
+            for (var i = 0; i < Buildings.Count; i++)
+                if (Buildings[i].BuildingId == buildingId) return Buildings[i].Damage;
+            return 0;
         }
     }
 }
