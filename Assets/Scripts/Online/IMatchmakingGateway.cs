6f8cbdf09449239c057f046dfa8258f546e56b60
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 23:18:48 +0200

    Add online authority contracts and validation

---
+++ Assets/Scripts/Online/IMatchmakingGateway.cs
@@ -0,0 +1,10 @@
+using System;
+namespace NewMaster.Online
+{
+    public interface IMatchmakingGateway
+    {
+        bool IsConnected { get; }
+        void FindMatch(string mode, Action<bool> completed);
+        void Cancel();
+    }
+}
