77a2c7d21e3afd416643dedaf49b2c4984103e2f
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 23:34:55 +0200

    Expand server command validation coverage

--- Assets/Tests/EditMode/ServerCommandValidatorTests.cs
+++ Assets/Tests/EditMode/ServerCommandValidatorTests.cs
@@ -1,5 +1,7 @@
+using System;
 using NUnit.Framework;
 using NewMaster.Online;
+
 namespace NewMaster.Tests
 {
     public sealed class ServerCommandValidatorTests
@@ -12,6 +14,37 @@ namespace NewMaster.Tests
             Assert.That(validator.IsValidCurrencyDelta(-1), Is.False);
             Assert.That(validator.IsValidWorldId(365), Is.True);
             Assert.That(validator.IsValidWorldId(366), Is.False);
+        }
+
+        [Test]
+        public void PlayerIdsRejectUnexpectedCharacters()
+        {
+            var validator = new ServerCommandValidator();
+
+            Assert.That(validator.IsValidPlayerId("player_01"), Is.True);
+            Assert.That(validator.IsValidPlayerId("player-01.test"), Is.True);
+            Assert.That(validator.IsValidPlayerId("player/01"), Is.False);
+            Assert.That(validator.IsValidPlayerId("player 01"), Is.False);
+        }
+
+        [Test]
+        public void ProductAndModeBoundsAreEnforced()
+        {
+            var validator = new ServerCommandValidator();
+
+            Assert.That(validator.IsValidProductId("coins.small"), Is.True);
+            Assert.That(validator.IsValidProductId(""), Is.False);
+            Assert.That(validator.IsValidMode("raid"), Is.True);
+            Assert.That(validator.IsValidMode(new string('x', 33)), Is.False);
+        }
+
+        [Test]
+        public void TimestampMustBeUtc()
+        {
+            var validator = new ServerCommandValidator();
+
+            Assert.That(validator.IsValidTimestamp(DateTime.UtcNow), Is.True);
+            Assert.That(validator.IsValidTimestamp(DateTime.Now), Is.False);
         }
     }
 }
